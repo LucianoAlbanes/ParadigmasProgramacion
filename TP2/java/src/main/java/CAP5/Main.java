b5e9894806e7e92a22e1dcb05e19cc01c1545734
LucianoAlbanes <26262333+LucianoAlbanes@users.noreply.github.com>
2021-08-31 17:02:48 -0300

    TP2.Java Cap5 (E19, E23)

--- TP2/java/src/main/java/CAP5/Main.java
+++ TP2/java/src/main/java/CAP5/Main.java
@@ -13,11 +13,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Print different options and get user choice.
-        System.out.printf("%s%n%s%n%s%n%s%n%s",
+        System.out.printf("%s%n%s%n%s%n%s%n%s%n%s",
                 "Insert the exercise number...",
                 "   1 - Exercise 5.8 (Parking slot)",
                 "   2 - Exercise 5.18 (Square of asterisks)",
                 "   3 - Exercise 5.19 (Square of characters)",
+                "   4 - Exercise 5.23 (Palindrome numbers)",
                 "Your choice: "
         );
         Scanner input = new Scanner(System.in);
@@ -28,6 +29,7 @@ public class Main {
             case 1 -> E8_Parking.main(null);
             case 2 -> E18_SquareAsterisks.main(null);
             case 3 -> E19_SquareAnyChar.main(null);
+            case 4 -> E23_PalindromeNum.main(null);
             default -> System.out.printf("'%d' isn't a valid option. Try again please.", userChoice);
         }
     }
